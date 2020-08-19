776842f1b4a0a71077f9f05596ee2ac4fc60b066
Akshat Zala <akshatzala@gmail.com>
2020-08-19 13:24:59 +0530

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import TicTacToe from './components/TicTacToe.vue';
+import Cell from '../components/Cell.vue';
+
+Vue.component('tic-tac-toe', TicTacToe);
+Vue.component('Cell', Cell);
+new Vue({
+  el: '#app',
+  render: h => {
+    return h(App);
+  }
+})
