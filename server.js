dd029551ff303af50bd0874ba1d1325c53d8932c
Akshat Zala <akshatzala@gmail.com>
2020-09-11 15:25:18 +0530

    Changed index.html

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // server.js
+
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
+let app = express();
+app.use(serveStatic("dist"));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
